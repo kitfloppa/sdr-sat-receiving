920cc4bc5be2ad6a6b9fefeebda81aab8959916c
kitfloppa <timofeev.anik@students.dvfu.ru>
2024-01-12 03:05:12 +1000

    add advance satelite WAV data processing

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import sys
 
 from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QFileDialog, QWidget, QVBoxLayout
 from PySide6.QtGui import QPixmap, QAction
+from PySide6 import QtCore
 
 from noaa_decoder import NOAADecoder
 from main_page import Ui_MainWindow
@@ -30,15 +31,16 @@ class MainWindow(QMainWindow):
         self.fname, _ = QFileDialog.getOpenFileName(self, 'Open File', '', 'WAV Files (*.wav)')
         
         if self.fname:
-            #self.pixmap = QPixmap(self.fname)
-            #self.label.setPixmap(self.pixmap)
-
             satellite_data = NOAADecoder(self.fname, 60)
 
             layout = QVBoxLayout(self.Graphic)
             layout.addWidget(satellite_data.get_signal_plot())
 
-            self.FileNameTitle.setText(satellite_data.image_path)
+            self.FileNameTitle.setText(satellite_data.img_save_path)
+
+            self.pixmap = QPixmap(satellite_data.img_save_path)
+            self.pixmap = self.pixmap.scaled(500, 500, QtCore.Qt.KeepAspectRatio)
+            self.SatelliteImage.setPixmap(self.pixmap)
 
 
 if __name__ == "__main__":
--- noaa_decoder.py
+++ noaa_decoder.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+from typing import List, Optional
 
 import os
 import matplotlib
@@ -6,13 +7,35 @@ import matplotlib
 matplotlib.use('Qt5Agg')
 
 import numpy as np
-import scipy.signal
 from scipy.io import wavfile
+from scipy.signal import hilbert, resample
 
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg, NavigationToolbar2QT
 from matplotlib.figure import Figure
-from matplotlib.image import imsave
+from PIL import Image
+
+
+# width of image components after reconstruction
+# see https://www.sigidwiki.com/wiki/Automatic_Picture_Transmission_(APT)
+COMPONENT_SIZES = {
+    "sync_a": (0, 39),
+    "space_a": (39, 86),
+    "image_a": (86, 995),
+    "telemetry_a": (995, 1040),
+    "sync_b": (1040, 1079),
+    "space_b": (1079, 1126),
+    "image_b": (1126, 2035),
+    "telemetry_b": (2035, 2080),
+}
+
+# sequence for alignment
+# https://www.sigidwiki.com/wiki/Automatic_Picture_Transmission_(APT)
+SYNCHRONIZATION_SEQUENCE = np.array([0, 0, 255, 255, 0, 0, 255, 255,
+                                     0, 0, 255, 255, 0, 0, 255, 255,
+                                     0, 0, 255, 255, 0, 0, 255, 255,
+                                     0, 0, 255, 255, 0, 0, 0, 0, 0,
+                                     0, 0, 0]) - 128
 
 
 class MplCanvas(FigureCanvasQTAgg):
@@ -24,79 +47,185 @@ class MplCanvas(FigureCanvasQTAgg):
 
 
 class NOAADecoder:
-    def __init__(self, file_path: str, norm: int) -> None:
-        #extract the data from WAV file
-        sampling_rate, self.signal_data = wavfile.read(file_path)
+    def __init__(self, file_path: str, black_point: int = 5, white_point: int = 95) -> None:
+        self.file_path = file_path
 
-        #normalization value which will be later used to bring the amplitudes to be in 0 to 255 range
-        am_envelope = self.hilbert(self.signal_data)
+        # load the audio and convert to Hilbert transformed amplitude info
+        self.audio_to_hilbert()
 
-        self.image_path = self.signal_to_img(sampling_rate, am_envelope, norm)
+        # sampling from the Hilbert transformed signal for desired images
+        self.subsample()
 
+        # digitize signal to valid pixel intensities in the uint8 range
+        self.quantize(black_point, white_point)
 
-    @staticmethod
-    def hilbert(signal_data: np.array) -> np.array:
-        #hilbert transfrom will give us an analytical signal
-		#this will help us extract the envelopes instantaneously
-		#for more info visit the following link
-		#https://dsp.stackexchange.com/questions/25845/meaning-of-hilbert-transform
+        # reshape the numpy array to a 2D image array
+        self.reshape()
 
-        #find the analytical signal
-        analytic_signal = scipy.signal.hilbert(signal_data)
+        # some empirically based filters for noisy rows
+        self.filter_noisy_rows()
 
-        #extract the amplitude envelope
-        am_envelope = np.abs(analytic_signal)
+        # select the image components to include
+        self.select_image_components()
 
-        return am_envelope
-    
-    @staticmethod
-    def signal_to_img(sampling_rate: int, am_envelope: np.array, norm: int) -> str:
-        #calculate the width and height of the image
-        width = int(sampling_rate * 0.5)
-        height = am_envelope.shape[0] // width
-
-        #create a numpy array with three channels for RGB and fill it up with zeroes
-        img_data = np.zeros((height, width, 3), dtype=np.uint8)
-
-        #keep track of pixel values
-        x, y = 0, 0
-
-        #traverse through the am_envelope and replace zeroes in numpy array with intensity values
-        for i in range(am_envelope.shape[0]):
-            #get the pixel intensity
-            intensity = int(am_envelope[i] // norm)
-
-            #make sure that the pixel intensity is between 0 and 255
-            if intensity < 0: intensity = 0
-            if intensity > 255: intensity = 255
-
-            #put the pixel on to the image
-            img_data[y][x] = intensity
-
-            x += 1
-
-            #if x is greater than width, sweep or jump to next line
-            if x >= width:
-                x, y = 0, y + 1
-
-                if y >= height:
-                    break
-        
         satellite_images_path = os.path.abspath(os.getcwd()) + '/satellite_images/'
-        img_path = satellite_images_path + 'sat_img.png'
+        self.img_save_path = satellite_images_path + 'sat_img.png'
 
         if not os.path.exists(satellite_images_path):
             os.mkdir(satellite_images_path)
         
-        imsave(img_path, img_data)
+        
+        img = Image.fromarray(self.image.astype(np.uint8))
+        img.save(self.img_save_path)
 
-        return img_path
+
+    def audio_to_hilbert(self, resample_rate: int = 20800) -> None:
+        '''
+        Load the audio and convert to Hilbert transformed amplitude info
+
+        :param in_path: string to path of audio file, relative or absolute
+        :param resample_rate: rate to resample audio at
+        :return: amplitude information corresponding to pixel intensity
+        '''
+
+        rate, self.audio = wavfile.read(self.file_path)
+
+        # resample audio at appropriate rate (20800)
+        coef = resample_rate / rate
+        samples = int(coef * len(self.audio))
+        self.audio = resample(self.audio, samples)
+
+        # if two-channel audio, average across channels
+        if self.audio.ndim > 1:
+            self.audio = np.mean(self.audio, axis=1)
+
+        # hilbert transform audio and extract envelope information
+        self.hilbert_transformed = np.abs(hilbert(self.audio))
+
+
+    def subsample(self, step: np.ndarray = 5) -> None:
+        self.subsampled = resample(self.hilbert_transformed, len(self.hilbert_transformed) // step)
+
+    
+    def quantize(self, black_point: int, white_point: int) -> np.ndarray:
+        '''
+        Digitize signal to valid pixel intensities in the uint8 range
+
+        :param arr: numpy array of continuous signal
+        :param black_point: dynamic range lower bound, percent
+        :param white_point: dynamic range upper bound, percent
+        :return: quantized numpy array to uint8
+        '''
+
+        #  percent for upper and lower saturation
+        low, high = np.percentile(self.subsampled, (black_point, white_point))
+
+        # range adjustment and quantization and cast to 8-bit range
+        self.quantized = np.round((255 * (self.subsampled - low)) / (high - low)).clip(0, 255).astype(np.uint8)
+    
+
+    def reshape(self,
+                synchronization_sequence: np.ndarray = SYNCHRONIZATION_SEQUENCE,
+                minimum_row_separation: int = 2000) -> None:
+        '''
+        Reshape the numpy array to a 2D image array
+
+        :param arr: 1D numpy array to arrange into 2D image array
+        :param synchronization_sequence: sequence to indicate row start
+        :param minimum_row_separation: minimum columns of separation
+            (a hair less than 2080)
+        :return: a 2D reshaped image array
+        '''
+
+        # initialize
+        rows, previous_corr, previous_ind = [None], -np.inf, 0
+
+        for current_loc in range(len(self.quantized) - len(synchronization_sequence)):
+
+            # proposed start of row, normalized to zero
+            row = [x - 128 for x in self.quantized[current_loc : current_loc + len(synchronization_sequence)]]
+
+            # correlation between the row and the synchronization sequence
+            temp_corr = np.dot(synchronization_sequence, row)
+
+            # if you're past the minimum separation, start hunting for the next synch
+            if current_loc - previous_ind > minimum_row_separation:
+                previous_corr, previous_ind = -np.inf, current_loc
+                rows.append(self.quantized[current_loc : current_loc + 2080])
+
+            # if the proposed region matches the sequence better, update
+            elif temp_corr > previous_corr:
+                previous_corr, previous_ind = temp_corr, current_loc
+                rows[-1] = self.quantized[current_loc : current_loc + 2080]
+
+        # stack the row to form the image, drop the incomplete rows at the end
+        self.reshaped = np.vstack([row for row in rows if len(row) == 2080])
+
+    
+    @staticmethod
+    def signal_to_noise(arr: np.ndarray, axis=0) -> np.ndarray:
+        """
+        Signal to noise (SNR) calculation (previously included in scipy)
+
+        :param arr: array to calculate signal to noise against
+        :param axis: axis to perform SNR calculation over
+        :return: signal to noise ratio calculated along axis
+        """
+
+        # mean and standard deviation along axis
+        mean, std = arr.mean(axis), arr.std(axis=axis)
+
+        # SNR calculation along axis
+        return np.where(std == 0, 0, mean / std)
+
+
+    def filter_noisy_rows(self) -> None:
+        '''
+        Some empirically based filters for noisy rows
+
+        :param arr: 2D image array
+        :return: image with filtered noisy rows
+        '''
+
+        # calculate signal to noise and the row to row difference in SNR
+        snr = self.signal_to_noise(self.reshaped, axis=1)
+        snr_diff = np.diff(snr, prepend=0)
+
+        # image filter for rows with high snr (pure noise) and minimal distance
+        # in SNR between rows (no theoretical basis, just seems to work)
+        self.denoised = self.reshaped[(snr > 0.6) & (snr_diff < 0.05) & (snr_diff > -0.05) & (snr_diff != 0), :]
+        #self.denoised = self.reshaped[:, :]
+
+
+    def select_image_components(self, 
+                                components: Optional[List[str]] = ['image_a', 'image_b']) -> None:
+        '''
+        Select the image components to include
+
+        :param arr: 2D image array
+        :param components: portions of the image to preserve/filter
+        :return: image array with just the appropriate image components
+        '''
+
+        # image array components
+        image_regions = []
+
+        # if there are no components, return the full image
+        if components is None:
+            return self.denoised
+
+        # image components to include, based on column down selection
+        for component in components:
+            component_start, component_end = COMPONENT_SIZES[component]
+            image_regions.append(self.denoised[:, component_start:component_end])
+
+        self.image = np.hstack(image_regions)
         
 
     def get_signal_plot(self) -> MplCanvas:
         fig = MplCanvas()
 
-        fig.axes.plot(self.signal_data)
+        fig.axes.plot(self.audio)
         # fig.axes.xlabel("Samples")
         # fig.axes.ylabel("Amplitude")
         # fig.axes.title("Signal")
